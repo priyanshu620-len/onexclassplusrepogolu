1437bffc3fc62e5204883e68d850acb937412382
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-22 23:29:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -786,7 +786,7 @@ async def txt_handler(bot: Client, m: Message):
             elif any(x in url for x in ["https://cpvod.testbook.com/", "classplusapp.com/drm/", "media-cdn.classplusapp.com", "media-cdn-alisg.classplusapp.com", "media-cdn-a.classplusapp.com", "tencdn.classplusapp", "videos.classplusapp", "webvideos.classplusapp.com"]):
                 # normalize cpvod -> media-cdn path used by API
                 url_norm = url.replace("https://cpvod.testbook.com/", "https://media-cdn.classplusapp.com/drm/")
-                api_url_call = f"https://mteckapi.vercel.app/ITsGOLU_OFFICIAL?url={url}"
+                api_url_call = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id=6157862068&token=615StAJfHYcWUVBZ4wL"
                 keys_string = ""
                 mpd = None
                 try:
